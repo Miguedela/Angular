284943008f61f15c9df01bc998d3b44313789a8d
DESKTOP-NQO1TN8\migue <miguelangeldelarosaleva@gmail.com>
2025-02-14 20:19:50 +0100

    update proyecto_angular

--- proyecto_angular/src/app/app.component.ts
+++ proyecto_angular/src/app/app.component.ts
@@ -8,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto_angular';
+  nombre = 'Miguel Ángel de la Rosa Leva'
+  edad = '21'
+  altura = '183'
+  puesto = 'programador'
+  adulto = true;
+
+  puestoTrabajo() {
+    return this.puesto;
+  }
+
+  esAdulto() {
+    return this.adulto ? 'Es adulto' : 'No es adulto';
+  }
 }
