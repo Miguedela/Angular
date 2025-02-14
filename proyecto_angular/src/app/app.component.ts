312367f02e86ea4500b389778b611b9816c36513
DESKTOP-NQO1TN8\migue <miguelangeldelarosaleva@gmail.com>
2025-02-14 21:15:16 +0100

    update proyecto_angular

--- proyecto_angular/src/app/app.component.ts
+++ proyecto_angular/src/app/app.component.ts
@@ -8,17 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto_angular';
-  nombre = 'Miguel Ángel de la Rosa Leva'
-  edad = '21'
-  altura = '183'
-  puesto = 'programador'
+  nombre = 'Miguel Ángel de la Rosa Leva';
+  edad = 18;
+  altura = 183;
+  puesto = 'programador';
   adulto = true;
 
-  puestoTrabajo() {
-    return this.puesto;
-  }
+  sueldos = [1400, 1300, 1200, 1100];
+  provincias = [
+    { id: '0', nombre: 'Sevilla' },
+    { id: '1', nombre: 'Cádiz' },
+    { id: '2', nombre: 'Málaga' },
+    { id: '3', nombre: 'Córdoba' }
+  ];
 
   esAdulto() {
     return this.adulto ? 'Es adulto' : 'No es adulto';
+  }
+
+  // eventos
+  diHola() {
+    alert("Hola amigo!");
+  }
+
+  valor = 0;
+
+  decrementar() {
+    this.valor--;
+  }
+
+  incrementar() {
+    this.valor++;
   }
 }
